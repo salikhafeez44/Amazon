0b3ac0e3ab4ba4d985b8f5946aa755458f7982d6
salikhafeez44 <47096464+salikhafeez44@users.noreply.github.com>
2020-07-16 23:59:38 -0500

    Initial commit

---
+++ Amazon/AmazonSolution.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace Amazon
+{
+    public class AmazonSolution
+    {
+        public double FindMedianSortedArrays(int[] nums1, int[] nums2)
+        {
+
+
+            return 0;
+        }
+
+        //public int LengthOfLongestSubstring(string s)
+        //{
+        //    //Going to use sliding Window technique
+        //    var a_pointer = 0;
+        //    var b_pointer = 0;
+        //    var max = 0;
+
+        //    HashSet<char> hashSet = new HashSet<char>();
+        //    if (s.Length == 1) { return 1; };
+
+        //    while (b_pointer < s.Length)
+        //    {
+        //        if (!hashSet.Contains(s[b_pointer]))
+        //        {
+        //            hashSet.Add(s[b_pointer]);
+        //            b_pointer++;
+
+        //            max = Math.Max(hashSet.Count, max);
+        //        }
+        //        else
+        //        {
+        //            hashSet.Remove(s[a_pointer]);
+        //            a_pointer++;
+        //        }
+        //    }
+        //    return max;
+
+        //}
+    }
+}
---
+++ Amazon/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Amazon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ////There are two sorted arrays nums1 and nums2 of size m and n respectively.
+            ////Find the median of the two sorted arrays. The overall run time complexity should be O(log (m+n)).
+            int[] num1 = { 0, 1, 2 };
+            int[] num2 = { 3, 4, 5 };
+            AmazonSolution amazonSolution = new AmazonSolution();
+            double median = amazonSolution.FindMedianSortedArrays(num1, num2);
+            Console.WriteLine("Median: {0}", median);
+
+
+
+            ////Given a string, find the length of the longest substring without repeating characters.
+            //string s = "pwwkew";
+            //AmazonSolution amazonSolution = new AmazonSolution();
+            //int count = amazonSolution.LengthOfLongestSubstring(s);
+            //Console.WriteLine("Count: {0}", count);
+        }
+    }
+}
