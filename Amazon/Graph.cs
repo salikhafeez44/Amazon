22b3de6f39cd5219cccf63f2cb6204c9d08970ad
salikhafeez44 <47096464+salikhafeez44@users.noreply.github.com>
2020-07-20 00:59:34 -0500

    Graph
    DFS and BFS Traversal

---
+++ Amazon/Graph.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Amazon
+{
+    public class Graph
+    {
+        private int Vertices { get; set; }
+
+        private List<int>[] AdjacencyList { get; set; }
+
+
+        public Graph(int v)
+        {
+            Vertices = v;
+            AdjacencyList = new List<int>[v];
+
+            for(int i = 0; i < Vertices; i++)
+            {
+                AdjacencyList[i] = new List<int>();
+            }
+        }
+
+        public void AddEdge(int v, int e)
+        {
+            AdjacencyList[v].Add(e);
+        }
+
+        public void PrintAdjacencyMatrix()
+        {
+            for(int i = 0; i < Vertices; i++)
+            {
+                foreach (var Item in AdjacencyList[i])
+                {
+                    Console.WriteLine(i + "-->" + Item);
+                    
+                }
+               
+            }
+
+        }
+
+        public void BFS(int start)
+        {
+            bool[] visited = new bool[Vertices];
+            Queue<int> queue = new Queue<int>();
+            visited[start] = true;
+            queue.Enqueue(start);
+
+            while (queue.Count!=0)
+            {
+                start = queue.Dequeue();
+                Console.WriteLine("next->" + start);
+
+                //Get all the adjacent vaertices of start
+                foreach(var Item in AdjacencyList[start])
+                {
+                    if (!visited[Item])
+                    {
+                        visited[Item] = true;
+                        queue.Enqueue(Item);
+                    }
+                }
+
+            }
+        }
+
+        public void DFS(int start)
+        {
+
+            bool[] visited = new bool[Vertices];
+            Stack<int> stack = new Stack<int>();
+            visited[start] = true;
+            stack.Push(start);
+
+            while (stack.Count != 0)
+            {
+                start = stack.Pop();
+                Console.WriteLine("next-->" + start);
+
+                foreach(var Item in AdjacencyList[start])
+                {
+                    if (!visited[Item])
+                    {
+                        visited[Item] = true;
+                        stack.Push(Item);
+                    }
+                    
+                }
+
+            }
+
+
+        }
+    }
+}
--- Amazon/Program.cs
+++ Amazon/Program.cs
@@ -6,13 +6,29 @@ namespace Amazon
     {
         static void Main(string[] args)
         {
+            //Graph BFS(Breadth First Search) and DFS (Depth Frist Search) Traversal
+            Graph graph = new Graph(4);
+            graph.AddEdge(0, 1);
+            graph.AddEdge(0, 2);
+            graph.AddEdge(1, 2);
+            graph.AddEdge(2, 0);
+            graph.AddEdge(2, 3);
+            graph.AddEdge(3, 3);
+
+            graph.PrintAdjacencyMatrix();
+
+            Console.WriteLine("BFS traversal starting from vertex 2:");
+            graph.BFS(2);
+            Console.WriteLine("DFS traversal starting from vertex 2:");
+            graph.DFS(2);
+
             ////There are two sorted arrays nums1 and nums2 of size m and n respectively.
             ////Find the median of the two sorted arrays. The overall run time complexity should be O(log (m+n)).
-            int[] num1 = { 0, 1, 2 };
-            int[] num2 = { 3, 4, 5 };
-            AmazonSolution amazonSolution = new AmazonSolution();
-            double median = amazonSolution.FindMedianSortedArrays(num1, num2);
-            Console.WriteLine("Median: {0}", median);
+            //int[] num1 = { 0, 1, 2 };
+            //int[] num2 = { 3, 4, 5 };
+            //AmazonSolution amazonSolution = new AmazonSolution();
+            //double median = amazonSolution.FindMedianSortedArrays(num1, num2);
+            //Console.WriteLine("Median: {0}", median);
 
 
 
